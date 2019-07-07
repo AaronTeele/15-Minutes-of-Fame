dd7125c5136907846c5f5aba692cbf48209cd335
aaronteele <aaronteele@pursuit.org>
2019-07-06 23:18:12 -0400

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline'
 import Home from './Home'
 
 class App extends Component {
     
     render(){
         return (
-            <div>
+            
+            <div className="App">
+                <CssBaseline />
                 <Home />
             </div>
         )
